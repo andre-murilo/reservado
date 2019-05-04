82cfb740b93564ca64bb4fd2f1ade1b83d46d2cd
wallace barbosa <43706747+wallacebarbosa@users.noreply.github.com>
2019-05-04 01:13:30 -0300

    Add files via upload

---
+++ template/pageInitial/js/urlparams.js
@@ -0,0 +1,24 @@
+class params {
+    constructor(name, value){
+        this.name = name;
+        this.value = value;
+    }
+}
+var params = [Cadartro]
+
+function getParams(){
+    let urlParams = new URLSearchParams(window.location.search);
+    let params = [];
+
+    for(var i = 0; i < parameters.length; i++)
+    {
+        if(urlParams.has(parameters[i]))
+        {
+            console.log(urlParams.get(parameters[i]));
+            params.push(new Params(parameters[i], urlParams.get(parameters[i])));
+        }
+    }
+
+
+    return params;
+}
